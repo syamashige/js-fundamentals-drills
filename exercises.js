198387567b50bcc06dc09f50ee8f7bbce5d7bc46
Sarah <yamashige.sarah@gmail.com>
2018-06-11 21:17:22 -1000

    getObjectKeys finished

--- exercises.js
+++ exercises.js
@@ -274,7 +274,9 @@ var countObjects = function(arr) {
  * @param {Object}
  * @return {Array}
  */
-var getObjectKeys;
+var getObjectKeys = function(obj) {
+  return Object.keys(obj);
+}
 
 /* #getObjectValues
  *
@@ -480,7 +482,7 @@ module.exports = {
   sumAllPositive: sumAllPositive,
   stringCountBelowThree: stringCountBelowThree,
   countObjects: countObjects,
-  getObjectKeys: null,
+  getObjectKeys: getObjectKeys,
   getObjectValues: null,
   makeObject: null,
   makeObjectReverse: null,
