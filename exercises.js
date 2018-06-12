cbb9e8a760db8d938d3d0981c984a0f7136cddce
Sarah <yamashige.sarah@gmail.com>
2018-06-11 18:58:33 -1000

    sumArrays finished

--- exercises.js
+++ exercises.js
@@ -25,7 +25,13 @@ doubleArray([1,2,3]);
  * @param {Array}
  * @return {Bool}
  */
-var sumArrays;
+var sumArrays = function(arr1, arr2) {
+  var sum = 0;
+  for (var i = 0;i < arr1.length; i++) {
+      sum += (arr1[i] + arr2[i]);
+  }
+  return sum;
+}
 
 /* #stringCount
  *
