ebd5c9cb659850ee36ce6ca422e13fac5fdd5daa
Sarah <yamashige.sarah@gmail.com>
2018-07-07 12:16:25 -1000

    finished sumBelowTen

--- exercises.js
+++ exercises.js
@@ -183,7 +183,15 @@ var lengthOfLast = function (str) {
  * @param {Array}
  * @return {Number}
  */
-var sumBelowTen;
+var sumBelowTen = function (arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < 10) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
 
 /* #moreThanTenLetters
  *
